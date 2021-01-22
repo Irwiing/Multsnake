a47be8d612d9086bcbe6aaa2dfe1b8dc48bbd116
Irwiing <irwingtguarnieri@gmail.com>
2021-01-22 18:35:08 -0300

    config

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ sockets.on('connection', (socket) => {
     })
 })
 
-server.listen(3000, () => {
+server.listen(process.env.PORT || 3000, () => {
     console.log('> server listening on port: 3000')
 })
